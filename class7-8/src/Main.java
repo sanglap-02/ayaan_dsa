e4473ef81f30b4ff8a17163dcda088b3f33e0149
sanglapk <sanglap.kundu@openlm.com>
2024-12-02 00:58:59 +0530

    class 7 and 8

--- class7-8/src/Main.java
+++ class7-8/src/Main.java
@@ -13,7 +13,10 @@ public class Main {
             next=null;
         }
 
+
     }
+
+
 
 
 
@@ -34,15 +37,30 @@ public class Main {
         listNode second=new listNode(2);
 
         listNode third=new listNode(3);
+        listNode fourth=new listNode(4);
 
 
         first.next=second;
         second.next=third;
+        third.next=fourth;
 
         // Traversing through a linkedList
 
-        printList(first);
+//        printList(first);
 
+//        System.out.println(lenght(first));
+
+//        addLast(first,new listNode(4));
+//        printList(first);
+
+//        listNode newHead=addFirst(first,new listNode(4));
+//
+//        printList(newHead);
+
+//        printList(first);
+
+        listNode newHead=insert(first,new listNode(5),2);
+        printList(newHead);
 
 
 
@@ -64,9 +82,91 @@ public class Main {
 
     }
 
-    static int lenght(listNode head){
+    static int lenght(listNode head) {
         //the lenght of the list
 
+        int count = 0;
+        listNode point = head;
+        while (point != null) {
+            count++;
+
+            point = point.next;
+        }
+        return count;
+    }
+
+    static void addLast(listNode head, listNode node) {
+        // 1->2->3 , 4
+        // 1->2->3->4
+
+        if (head == null) {
+            head = node;
+            return;
+        }
+
+        listNode point = head;
+        while (point.next != null) {
+            point = point.next;
+        }
+
+        point.next = node;
+
+    }
+
+    static listNode addFirst(listNode head, listNode node) {
+        // 1->2->3 , 4
+        // 4->1->2->3
+
+        if (head == null) {
+            head = node;
+            return head;
+        }
+
+        node.next =head;
+
+        return node;
+
+//        return point;
+
+    }
+
+    static listNode insert(listNode head, listNode node, int index){
+
+        // if head is null
+        if(head==null){
+            if(index==0) return node;
+            return null;
+        }
+
+        //index =0 ;
+        if(index==0) {
+            listNode newHaed=addFirst(head,node);
+            return newHaed;
+        }
+
+        // index=length
+        int length=lenght(head);
+        if(index==length){
+            addLast(head,node);
+            return head;
+        }
+
+        listNode point=head;
+        int ind=0;
+        while (ind!=index-1){
+//            System.out.println("the value of ind is "+ind);
+            point=point.next;
+            ind++;
+        }
+
+//        System.out.println(point.data);
+
+        listNode temp=point.next;
+
+        point.next=node;
+        node.next=temp;
+
+        return head;
 
     }
 
